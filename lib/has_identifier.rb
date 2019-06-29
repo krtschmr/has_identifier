d9bb6ddf12dd0721b89545fa6e70627d8af32b03
Tim Kretschmer <tim@krtschmr.de>
2019-06-29 19:05:54 +0700

    refactor

--- lib/has_identifier.rb
+++ lib/has_identifier.rb
@@ -3,14 +3,30 @@ module HasIdentifier
 
   included do
     class_attribute :identifier_prefix
-
-    validates(:identifier, presence: true)
-
-    after_initialize{
-      def r(i); SecureRandom.send(:choose, [*?1..?9, *?A..?H, *?J..?N, *?P..?Z], i); end
-      self.identifier ||= "#{identifier_prefix}#{r(5)}-#{r(5)}-#{r(6)}"
-    }
+    after_initialize :generate_identifier
+    validates :identifier, presence: true, uniqueness: { allow_blank: false,case_sensitive: false}, on: :create
   end
+
+  ALPHABET = [*?1..?9, *?A..?H, *?J..?N, *?P..?Z]
+
+  def gen_id_char(n)
+    SecureRandom.send(:choose, ALPHABET, n)
+  end
+
+  def generate_identifier
+    self.identifier ||= "#{identifier_prefix}#{gen_id_char(5)}-#{gen_id_char(5)}-#{gen_id_char(6)}"
+  end
+
+  # included do
+  #   class_attribute :identifier_prefix
+  #
+  #   validates(:identifier, presence: true)
+  #
+  #   after_initialize{
+  #     def r(i); SecureRandom.send(:choose, [*?1..?9, *?A..?H, *?J..?N, *?P..?Z], i); end
+  #     self.identifier ||= "#{identifier_prefix}#{r(5)}-#{r(5)}-#{r(6)}"
+  #   }
+  # end
 end
 
 
