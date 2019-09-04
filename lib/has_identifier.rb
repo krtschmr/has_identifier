9c7d3ee63582eafad532f036c0a9730a8bd9d0ed
Tim Kretschmer <tim@krtschmr.de>
2019-09-04 04:33:30 +0000

    add finders by identifier

--- lib/has_identifier.rb
+++ lib/has_identifier.rb
@@ -4,10 +4,21 @@ module HasIdentifier
   included do
     class_attribute :identifier_prefix
     after_initialize :generate_identifier
-    validates :identifier, presence: true, uniqueness: { allow_blank: false,case_sensitive: false}, on: :create
+    validates :identifier, presence: true, uniqueness: { allow_blank: false, case_sensitive: false}, on: :create
+
+    def self.find(*args)
+      # if the argument is a string but it's not a string with just numbers
+      if args.first.is_a?(String) && args.first.to_i == 0
+        find_by!(identifier: args)
+      else
+        super
+      end
+    end
+
   end
 
   ALPHABET = [*?1..?9, *?A..?H, *?J..?N, *?P..?Z]
+
 
 
   def short_identifier
@@ -28,5 +39,16 @@ ActiveRecord::ConnectionAdapters::TableDefinition.class_eval do
   # usage of t.identifier inside migrations
   def identifier
     column(:identifier, :string, length: 19, null: false, index: true, unique: true)
+  end
+end
+
+
+ActiveRecord::Associations::CollectionAssociation.class_eval do
+  def find(*args)
+    if args.first.is_a?(String) && args.first.to_i == 0
+      scope.find_by!(identifier: args)
+    else
+      super
+    end
   end
 end
